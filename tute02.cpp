1cbb61b1ae88133d94f7f09774fbfea2ef47f02f
it21033032 <87374999+it21033032@users.noreply.github.com>
2021-07-21 05:43:10 +0000

    convert into c++

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,78 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
-{
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
+// #include <stdio.h>
+// int main()
+// {
+//    double salary, netSalary;
+//    int etype, otHrs, otRate;
+//    printf("Enter Employee Type : ");
+//    scanf("%d", &etype);
+//    printf("Enter Salary  : ");
+//    scanf("%f", &salary);
+//    printf("Enter OtHrs : ");
+//    scanf("%d", &otHrs); 
    
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
+//    switch (etype) {
+//       case 1 :
+//           otRate = 1000;
+//           break;
+//       case 2 :
+//           otRate = 1500;
+//           break;
+//       default :
+//           otRate = 1700;
+//           break;
+//    }
 
 
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+//    netSalary = salary + otHrs* otRate;
+//    printf("Net Salary is %f ", netSalary);
   
-   return 0;
+//    return 0;
+// }
+
+#include<iostream>
+int main(){
+double salary,netSalary;
+int etype,otRate,otHrs;
+
+std::cout<<"enter employee type :";
+std::cin>>etype;
+std::cout<<"enter the salary :";
+std::cin>>salary;
+std::cout<<"enter the ot Hours :";
+std::cin>>otHrs;
+
+switch(etype){
+case 1:otRate = 1000;
+break;
+
+case 2:otRate = 1500;
+break;
+
+default : otRate = 1700;
+break;
+
 }
+
+netSalary = salary + otHrs*otRate;
+std::cout<<"net salary is "<<netSalary;
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
